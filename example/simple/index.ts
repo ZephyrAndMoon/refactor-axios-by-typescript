441b70485321cbdcc88348a2ee65d23129536731
Winds0605 <1024676057@qq.com>
2019-10-18 11:01:08 +0800

    feat(some files): 实现了get请求以及url参数拼接
    实现了get请求以及url参数拼接,支持多种参数拼接

---
+++ example/simple/index.ts
@@ -0,0 +1,11 @@
+
+import axios from '../../src/index'
+
+axios({
+  method: 'get',
+  url: '/simple/get',
+  data: {
+    a:1,
+    b:2
+  }
+})
---
+++ src/helpers/url.ts
@@ -0,0 +1,62 @@
+import { isDate, isPlainObject } from './util'
+
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+    // 如果不是有效值则跳出这次forEach循环进入下一次
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+    let values = []
+
+    if (Array.isArray(val)) {
+      values = val
+      // url中参数key值为数组的时候需要在后面加上[]
+      // 例如 http://localhost:8080/e3izm/NavimenuitemController/selectAll?nmiids[]=1&nmiids[]=2&pageno=1&pagesize=8
+      key += '[]'
+    } else {
+      // 方便下面做遍历
+      values = [val]
+    }
+
+    values.forEach(val => {
+      if (isDate(val)) {
+        // 转化为url中时间格式
+        val.toISOString()
+      } else if (isPlainObject(val)) {
+        val = JSON.stringify(val)
+      }
+
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+    // 有问号说明后面已经有参数，可以直接加 '&' 再拼接上遍历后得到的参数
+    // 没问号加上 '?' 再拼接参数
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+  return url
+}
